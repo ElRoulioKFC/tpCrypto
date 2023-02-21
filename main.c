63618d2860ed52a8c1eedea1ce8b9ed9d1d2dbf4
roulio <julesgeyer@outlook.fr>
2023-02-21 10:57:41 +0100

    test

--- main.c
+++ main.c
@@ -1,5 +1,7 @@
 #include<stdlib.h>
 #include <openssl/md5.h>
+#include <stdio.h>
+#include <string.h>
 
 typedef unsigned char byte;
 
@@ -8,9 +10,15 @@ void hash_MD5(const char* s, byte* empreinte)
     MD5((unsigned char*)s, strlen(s), empreinte);
 }
 
+void hash_SHA1(const char* s, byte* empreinte)
+{
+    SHA1((unsigned char*)s, strlen(s), empreinte);
+}
+
 //function main to create MD5 hash
 int main(int argc, char *argv[]) {
     char* test = "Salut";
-    hash_MD5(test, "6186484864161613");
+    hash_MD5(test, char name[MD5_DIGEST_LENGTH]);");
     printf(test);
+    return 0;
 }

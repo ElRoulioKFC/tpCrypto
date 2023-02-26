54dc00cae3769b9177106117c97ac7705a13bb16
Thomas <thomas0bellon@gmail.com>
2023-02-26 18:07:41 +0100

    h2i done

--- README.md
+++ README.md
@@ -5,3 +5,15 @@ Le test se lance automatiquement avec le programme.
 
 ## Question 2
 Le test se lance automatiquement avec le programme. 
+
+## Question 3
+
+## Question 4
+
+## Question 5
+
+## Question 6
+
+## Question 7
+
+La variable t permet d'augmenter la couverture de la table en utilisant des indices aléatoires.
--- main.c
+++ main.c
@@ -16,8 +16,35 @@ int min_size = -1;
 int max_size = -1;
 int64_t N;
 int i2c_called = 0;
+int i2c_num;
 char* texte_clair_i2i = "";
-int largeurChaine;
+unsigned char* md5_res = "";
+byte md5_em[MD5_DIGEST_LENGTH];
+unsigned char* sha1_res = "";
+byte sha1_em[SHA_DIGEST_LENGTH];
+int h2i_called = 0;
+
+void hash_MD5(char* s, byte* empreinte)
+{
+    MD5((unsigned char*)s, strlen(s), empreinte);
+}
+
+void hash_SHA1(const char* s, byte* empreinte)
+{
+    SHA1((unsigned char*)s, strlen(s), empreinte);
+}
+
+void hash_MD5_e(char* s)
+{
+    byte empreinte[MD5_DIGEST_LENGTH];
+    MD5((unsigned char*)s, strlen(s), empreinte);
+}
+
+void hash_SHA1_e(char* s)
+{
+    byte empreinte[SHA_DIGEST_LENGTH];
+    SHA1((unsigned char*)s, strlen(s), empreinte);
+}
 
 char* i2c_naif(int64_t i, int size) {
     // init
@@ -53,16 +80,11 @@ char* i2c(int64_t value){
     return i2c_naif(tmp_val, size);
 }
 
-/*
-uint64_t randomIndice(){
-    unsigned long n1 = rand();
-    unsigned long n2 = rand();
-    return ( (uint64) n2 )+ ( ( (uint64) n1 ) << 32 );
-}*/
-
-uint64_t h2i(byte empreinte, int t) {
-    uint64_t* empreinteInt = (uint64_t*) empreinte;
-    uint64_t i = (empreinteInt[0] + t) % largeurChaine;
+uint64_t h2i(unsigned char* y, int t) {
+    unsigned char y_8[8];
+    sscanf(y, "%2hhx%2hhx%2hhx%2hhx%2hhx%2hhx%2hhx%2hhx", &y_8[0], &y_8[1], &y_8[2], &y_8[3], &y_8[4], &y_8[5], &y_8[6], &y_8[7]);
+    uint64_t y_64 = *((uint64_t*) y_8);
+    return (y_64 + t) % N;
 }
 
 uint64_t i2i(uint64_t idx, int t){
@@ -79,17 +101,6 @@ uint64_t i2i(uint64_t idx, int t){
     return indice;
 }
 
-uint64_t nouvelle_chaine(uint64_t idx1, int largeur){
-    /*Calcul une chaîne de h et d'indices de longeure largeur, ne renvoie que le dernier indice*/
-    uint64_t res;
-    largeurChaine = largeur;
-    for (int i = 1; i < largeur; i++)
-    {
-        res = i2i(idx1, i);
-    }
-    return res;
-}
-
 void init(){
     N = 0;
     int size_alph = strlen(alphabet);
@@ -98,21 +109,6 @@ void init(){
     }
     // printf("%ld", N);
     // printf("\n");
-}
-
-
-unsigned char *MD5(const unsigned char *d, 
-                   unsigned long n,
-                   unsigned char *md);
-
-void hash_MD5(char* s, byte* empreinte)
-{
-    MD5((unsigned char*)s, strlen(s), empreinte);
-}
-
-void hash_SHA1(const char* s, byte* empreinte)
-{
-    SHA1((unsigned char*)s, strlen(s), empreinte);
 }
 
 void test_md5(char* testString){
@@ -130,10 +126,16 @@ void test_sha1(char* testString){
     hash_SHA1(testString, empreinte);
     for(int i = 0; i < SHA_DIGEST_LENGTH ; i++ ){
         printf("%0x", empreinte[i]);
-        res += empreinte[i];
     }
     printf("\n");
-    return res;
+}
+
+void bin_to_char(unsigned char* tmp, byte* em, int length){
+    char* p = tmp;
+    for(int i = 0; i < length ; i++ ){
+        sprintf(p, "%0x", em[i]);
+        p += 2;
+    }
 }
 
 
@@ -150,6 +152,7 @@ int main(int argc, char *argv[]) {
         {"md5", required_argument, 0, 'm'},
         {"sha1", required_argument, 0, 's'},
         {"i2c", required_argument, 0, 'x'},
+        {"h2i", no_argument, 0, 'y'},
         {0, 0, 0, 0}
     };
 
@@ -168,17 +171,23 @@ int main(int argc, char *argv[]) {
                 alphabet_size = strlen(alphabet);
                 break;
             case 'm' :
-                printf("test MD5 : %s\n", optarg);
-                test_md5(optarg);
+                md5_res = optarg;
+                printf("MD5 : %s\n", md5_res);
+                test_md5(md5_res);
                 break;
             case 's' :
-                printf("test SHA1 : %s\n", optarg);
-                test_sha1(optarg);
+                sha1_res = optarg;
+                printf("SHA1 : %s\n", sha1_res);
+                test_sha1(sha1_res);
                 break;
             case 'x' :
                 i2c_called = 1;
                 i2c_num = atoi(optarg);
                 break;
+            case 'y' :
+                h2i_called = 1;
+                break;
+
         } 
     }
 
@@ -189,5 +198,18 @@ int main(int argc, char *argv[]) {
             char* i2cResult = i2c(i2c_num);
             printf("\nI2C --> %s\n", i2cResult);
         }
+    }
+
+    if(strlen(md5_res) != 0 && h2i_called){
+        unsigned char tmp[MD5_DIGEST_LENGTH * 2];
+        hash_MD5(md5_res, md5_em);
+        bin_to_char(tmp, md5_em, MD5_DIGEST_LENGTH);
+        printf("H2I '%s' --> %ld\n", tmp, h2i(tmp, 1));
+    }
+    else if(strlen(sha1_res) != 0 && h2i_called){
+        unsigned char tmp[SHA_DIGEST_LENGTH * 2];
+        hash_MD5(sha1_res, sha1_em);
+        bin_to_char(tmp, sha1_em, SHA_DIGEST_LENGTH);
+        printf("H2I '%s' --> %ld\n", tmp, h2i(tmp, 1));
     }
 }
